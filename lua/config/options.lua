c16af0fa6b871b620f6d33518d4ed00b190b7797
Cory Zimmerman <corzimmerman@tesla.com>
2025-07-26 22:45:47 -0700

    sync

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,15 +6,23 @@ vim.opt.wrap = true
 vim.g.root_spec = { "cwd" }
 vim.opt.clipboard = 'unnamedplus'
 
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  },
-}
+if vim.env.SSH_TTY then
+  local function paste(_)
+    return function()
+      local content = vim.fn.getreg('"')
+      return vim.split(content, "\n")
+    end
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste("+"),
+      ["*"] = paste("*"),
+    },
+  }
+end
 
